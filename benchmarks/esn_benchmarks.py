a612741278a9eba83c8c7cfa3c6bfd1b6786a11d
williamgilpin <williamgilpin@gmail.com>
2023-01-24 22:35:10 -0600

    latest

--- benchmarks/esn_benchmarks.py
+++ benchmarks/esn_benchmarks.py
@@ -14,19 +14,27 @@ from darts import TimeSeries
 import dysts
 import dysts.flows
 from dysts.base import get_attractor_list
-from dysts.datasets import load_file
+from dysts.datasets import load_file, convert_json_to_gzip
 
 
 from resources.esn import ESNForecast
 
 SEED = 0
+LONG_TEST = True
+
 niters = 500
 
 cwd = os.path.dirname(os.path.realpath(__file__))
 # cwd = os.getcwd()
 input_path_train = os.path.dirname(cwd)  + "/dysts/data/train_multivariate__pts_per_period_100__periods_12.json.gz"
 input_path_test = os.path.dirname(cwd)  + "/dysts/data/test_multivariate__pts_per_period_100__periods_12.json.gz"
-output_path = cwd + "/results/results_neural_esn_multivariate.json"
+if LONG_TEST:
+    input_path_test = os.path.dirname(cwd)  + "/dysts/data/test_multivariate__pts_per_period_100__periods_60.json.gz"
+
+output_path = cwd + "/results/results_esn_multivariate.json"
+if LONG_TEST:
+    output_path = cwd + "/results/results_esn_multivariate__pts_per_period_100__periods_60.json"
+
 
 equation_data_train = load_file(input_path_train)
 equation_data_test = load_file(input_path_test)
@@ -64,6 +72,7 @@ for equation_name in get_attractor_list():
     t_train, sol_train = np.arange(len(y_train)), y_train
     t_test, sol_test = np.arange(len(y_val)), y_val
 
+    ## Validation
     all_scores = list()
     for leak_rate in leak_rates:
         try:
@@ -75,17 +84,20 @@ for equation_name in get_attractor_list():
             continue
         score_val = score_func(sol_test, sol_pred)
         all_scores.append(score_val)
-
     leak_rate_opt = leak_rates[np.argmin(all_scores)]
     print(leak_rate_opt)
 
+
+    ## Inference
     test_data = np.copy(np.array(equation_data_test.dataset[equation_name]["values"]))
-    split_point = int(5 / 6 * len(test_data))
+    split_point = int(1 / 6 * len(test_data))
+    if LONG_TEST:
+        split_point = int(1 / 6 * len(test_data))
     y_test, y_test_val = test_data[:split_point], test_data[split_point:]
 
     model = ESNForecast(test_data.shape[-1], leak_rate=leak_rate_opt, random_state=SEED)
     model.fit(y_test)
-    y_test_pred_val = model.predict(200)
+    y_test_pred_val = model.predict(len(y_test_val))
     score_val = score_func(y_test_val, y_test_pred_val)
 
     all_results[equation_name] = dict()
@@ -97,6 +109,8 @@ for equation_name in get_attractor_list():
     print(equation_name, score_val, flush=True)
     with open(output_path, 'w') as f:
         json.dump(all_results, f, indent=4, sort_keys=True)   
+
+convert_json_to_gzip(output_path)
         
 
 
