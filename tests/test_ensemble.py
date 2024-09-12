179900539879bcbf1d6780132f05d69f107e0f6d
Jeffrey Lai <jlai@utexas.edu>
2024-09-12 00:34:56 -0500

    nit: update ensemble test with transforms

--- tests/test_ensemble.py
+++ tests/test_ensemble.py
@@ -1,9 +1,13 @@
-from dysts.systems import make_trajectory_ensemble
+from dysts.systems import (
+    gaussian_init_cond_sampler,
+    gaussian_parameter_sampler,
+    make_trajectory_ensemble,
+)
 
 
 def main():
     sampler = gaussian_init_cond_sampler()
-    pt = ParamPerturb(1e-4, random_seed=9999)
+    pt = gaussian_parameter_sampler(1e-4, random_seed=9999)
     make_trajectory_ensemble(
         100,
         use_multiprocessing=True,
