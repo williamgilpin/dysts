edcb93062f55f36337d3e48fcdd34a91f86d8f7e
Jeffrey Lai <jlai@utexas.edu>
2024-09-13 16:40:38 -0500

    integrate ddeint implementation + refactor utils into separated folder

--- tests/test_delay.py
+++ tests/test_delay.py
@@ -5,6 +5,8 @@ def main():
     sys = MackeyGlass()
     sol = sys.make_trajectory(1024, pts_per_period=1024 // 10, standardize=True)
 
+    print(sol.shape)
+
 
 if __name__ == "__main__":
     main()
