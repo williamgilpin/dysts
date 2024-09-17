1b504a7a967f515eb7ac8380cc4733a219ab5570
Anthony Bao <anthonybao1999@gmail.com>
2024-09-17 17:25:21 -0500

    updated samplers to use events

--- dysts/sampling.py
+++ dysts/sampling.py
@@ -1,7 +1,7 @@
 """Sampling functions for dysts"""
 
 from dataclasses import dataclass, field
-from typing import Dict, Optional
+from typing import Dict, Optional, Callable, List
 
 import numpy as np
 from numpy.typing import NDArray
@@ -27,13 +27,25 @@ class BaseSampler:
 
 @dataclass
 class GaussianInitialConditionSampler(BaseSampler):
-    """Sample gaussian perturbations for each initial condition in a given system list"""
+    """
+    Sample gaussian perturbations for each initial condition in a given system list
+    """
 
     scale: float = 1e-4
+    verbose: bool = False # for testing purposes
 
     def __call__(self, ic: Array, system: Optional[BaseDyn] = None) -> Array:
+        # Scale the covariance relative to each dimension
         scaled_cov = np.diag(np.square(ic * self.scale))
-        return self.rng.multivariate_normal(mean=ic, cov=scaled_cov)
+        perturbed_ic = self.rng.multivariate_normal(mean=ic, cov=scaled_cov)
+
+        if self.verbose:
+            if system is not None:
+                print(f"System: {system.name}")
+            print(f"--> Original initial condition: {ic}")
+            print(f"--> Perturbed initial condition: {perturbed_ic}")
+
+        return perturbed_ic
 
 
 @dataclass
@@ -46,25 +58,49 @@ class OnAttractorInitCondSampler(BaseSampler):
           parameters before sampling from the attractor.
         - The sampled initial conditions from this sampler are necessarily
           tied to the attractor defined by the default parameters.
+    
+    Args:
+        reference_traj_length: Length of the reference trajectory to use for sampling ic on attractor.
+        reference_traj_transient: Transient length to ignore for the reference trajectory
+        trajectory_cache: Cache of reference trajectories for each system.
+        events: events to pass to solve_ivp
     """
 
     reference_traj_length: int = 4096
     reference_traj_transient: int = 500
     trajectory_cache: Dict[str, Array] = field(default_factory=dict)
+    verbose: bool = False # for testing purposes
+    events: Optional[List[Callable]] = None # solve_ivp events
 
     def __call__(self, ic: Array, system: BaseDyn) -> Array:
         if system.name is None:
             raise ValueError("System must have a name")
+        
+        # make reference trajectory if not already cached
         if system.name not in self.trajectory_cache:
             # Integrate the system with default parameters
-            self.trajectory_cache[system.name] = system.make_trajectory(
-                self.reference_traj_length
+            reference_traj = system.make_trajectory(
+                self.reference_traj_length,
+                events=self.events,
+                verbose=self.verbose,
             )[self.reference_traj_transient :]
+
+            if reference_traj is None: # if integrate fails, resulting in an incomplete trajectory
+                raise ValueError(f"Failed to integrate the system {system.name} with ic {system.ic} and params {system.params}")
+
+            self.trajectory_cache[system.name] = reference_traj
 
         trajectory = self.trajectory_cache[system.name]
 
         # Sample a new initial condition from the attractor
-        return self.rng.choice(trajectory)
+        new_ic = self.rng.choice(trajectory)
+
+        if self.verbose:
+            print(f"System: {system.name}")
+            print(f"--> Original initial condition: {ic}")
+            print(f"--> New initial condition: {new_ic}")
+
+        return new_ic
 
 
 @dataclass
@@ -81,6 +117,7 @@ class GaussianParamSampler(BaseSampler):
     """
 
     scale: float = 1e-2
+    verbose: bool = False # for testing purposes 
 
     def __call__(
         self, name: str, param: Array, system: Optional[BaseDyn] = None
@@ -92,11 +129,18 @@ class GaussianParamSampler(BaseSampler):
         flat_param = np.array(param).flatten()
         scale = np.abs(flat_param) * self.scale
         cov = np.diag(np.square(scale))
-        out = (
+        perturbed_param = (
             self.rng.multivariate_normal(mean=flat_param, cov=cov)
             .reshape(shape)
             .squeeze()
         )
         if isinstance(param, (float, int)):
-            out = float(out)
-        return out
+            perturbed_param = float(perturbed_param)
+    
+        if self.verbose:
+            print(f"System: {system.name}")
+            print(f"Parameter name: {name}")
+            print(f"--> Original parameter: {param}")
+            print(f"--> Perturbed parameter: {perturbed_param}")
+
+        return perturbed_param
