c5ce211a9e46e2e4c7399a365346956526e26333
Jeffrey Lai <jlai@utexas.edu>
2024-09-18 19:27:54 -0500

    cleanup useless vars in __init__

--- dysts/__init__.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-import pkg_resources
-
-data_path = pkg_resources.resource_filename("dysts", "data/chaotic_attractors.json")
-data_path2 = pkg_resources.resource_filename("dysts", "data/discrete_maps.json")
-data_dirpath = pkg_resources.resource_filename("dysts", "data")
-PACKAGEDIR = Path(__file__).parent.absolute()
